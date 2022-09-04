3cf5e77945a1d631e29d664f43bf6591ad97c27b
Peqchji <jirapat3774@gmail.com>
2022-09-04 20:12:31 +0700

    finished

---
+++ Week8_Sol2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    printf("Before process\nnum1 = %d, num2 = %d", num1, num2);
+    num1 = num1 * num2;//num1 = num1 + num2;
+    num2 = num1 / num2;//num2 = num1 - num2;
+    num1 = num1 / num2;//num1 = num1 - num2;
+    printf("\nAfter process\nnum1 = %d, num2 = %d", num1, num2);
+}
